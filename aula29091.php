0c381b07e1047343e62f41fdddbcbe8cf9e40cfa
aninha998765 <130669141+aninha998765@users.noreply.github.com>
2023-09-29 08:59:30 -0300

    commitzinatto

---
+++ aula29091.php
@@ -0,0 +1,28 @@
+<?php
+
+$valor1 = 5;
+$valor2 = 2;
+$nome1 = "Robson";
+$nome2 = "Pedro";
+$valor3 = 12;
+$valor4 = 11;
+
+if ($valor1 > $valor2) {
+    echo "5 é maior que 2. <br>";
+} else {
+    echo "5 não é maior que 2. <br>";
+}
+
+if ($nome1 != $nome2) {
+    echo "Robson é diferente de Pedro. <br>";
+} else {
+    echo "Robson não é diferente de Pedro. <br>";
+}
+
+if ($valor3 <= $valor4) {
+    echo "12 é menor ou igual a 11. <br>";
+} else {
+    echo "12 não é menor ou igual a 11. <br>";
+}
+
+?>
