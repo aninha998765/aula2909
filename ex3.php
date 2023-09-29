104ff29dfe7590b3ee3a5c9bc873ebdedff058cc
aninha998765 <130669141+aninha998765@users.noreply.github.com>
2023-09-29 12:02:43 -0300

    commit zinatto

---
+++ ex3.php
@@ -0,0 +1,19 @@
+<?php
+
+//declaração de variáveis
+$num1 = 56;
+$num2 = 5;
+
+//declaração de pessoa
+$pes1 = "Laura";
+
+//identifica se a variável é um numero
+if (is_int($num1)){
+    $num3 = $num1 * 2;
+} if ($num3 > 100){
+    echo "é maior de 100";
+} else {
+    echo "Não é maior que 100";
+}
+
+?>
---
+++ ex4.php
@@ -0,0 +1,10 @@
+<?php
+$numero1 = 30;
+
+if ($numero1 >= 51) {
+    echo "Você receberá uma multa.";} 
+    elseif ($numero1 >= 41 && $numero1 < 50) {
+    echo "A velocidade está correta";}
+    elseif ($numero1 = 40) { 
+    echo "Atenção com a velocidade";}
+?>
